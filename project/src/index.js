be79535432d7d489adc09b6d1a3b760d1b2e66ac
Alexander Gorham <agram@hotmail.co.uk>
2023-12-19 01:26:31 +0100

    Desktop navbar set up

--- project/src/index.js
+++ project/src/index.js
@@ -2,13 +2,5 @@ import assignEventListeners from "./assignEventListeners";
 import imageCarousel from "./imageCarousel";
 
 
-assignEventListeners(
-    ".menu-icon", 
-    ".menu-element",
-    ".dropdown-content", 
-    "dropdown-content-display", 
-    "dropdown-content-hidden" 
-    );
 
-    imageCarousel();
 
